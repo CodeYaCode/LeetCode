038e0a3a0764b25d0b300f3716d716940b8c67e3
LiuChen <564127840@qq.com>
2017-03-15 12:09:38 +0800

    'First' commit.

---
+++ src/com/leetcode/chen/main/TwoSum/Solution.java
@@ -0,0 +1,18 @@
+/*
+ * Two Sum
+ */
+package com.leetcode.chen.main.TwoSum;
+
+/**
+ * @Title Two Sum
+ * @Description
+ * Given an array of integers, return indices of the two numbers 
+ * such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, 
+ * and you may not use the same element twice.
+ * 
+ * @author LiuChen
+ */
+public class Solution {
+    
+}
