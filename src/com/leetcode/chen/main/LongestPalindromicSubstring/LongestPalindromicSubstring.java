f8b13fc011381859e2de0624117e64b1bfd11fb2
LiuChen <564127840@qq.com>
2018-03-20 17:31:15 +0800

    ReverseInteger

---
+++ src/com/leetcode/chen/main/LongestPalindromicSubstring/LongestPalindromicSubstring.java
@@ -0,0 +1,68 @@
+/**
+ * Longest palindromic substring
+ */
+package com.leetcode.chen.main.LongestPalindromicSubstring;
+
+/**
+ * @Title Longest palindromic substring
+ * @Description
+ *  Given a string s, find the longest palindromic substring in s. 
+ *  You may assume that the maximum length of s is 1000.
+ * @Example
+ * 
+ *  Example:
+ *  Input: "babad"
+ *  Output: "bab"
+ *  Note: "aba" is also a valid answer.
+ *  
+ *  Example:
+ *  Input: "cbbd"
+ *  Output: "bb"
+ * @author LiuChen
+ *
+ */
+public class LongestPalindromicSubstring {
+    public String longestPalindrome(String s) {
+        int max = 0, start = 0, length = s.length();
+        String ans = s;
+        while(max < ans.length()) {
+            if (isPalindrome(ans)) {
+                max = ans.length();
+                break;
+            }
+            ans = s.substring(start, --length);
+        }
+        return ans;
+    }
+    
+    public boolean isPalindrome(String s) {
+        if (null == s || 1 <= s.length()) {
+            return true;
+        }
+        if (2 == s.length()) {
+            if (s.substring(0, 1).equals(s.substring(1))) {
+                return true;
+            }
+            return false;
+        }
+        if (3 == s.length()) {
+            if (s.substring(0, 1).equals(s.substring(2))) {
+                return true;
+            }
+            return false;
+        }
+        if (s.substring(0, 1).equals(s.substring(s.length()-1, s.length()))) {
+            return isPalindrome(s.substring(1, s.length()-1));
+        }
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        LongestPalindromicSubstring l = new LongestPalindromicSubstring();
+        String s = "babad";
+        System.out.println(s.substring(0, 1));
+        System.out.println(s.substring(s.length()-1, s.length()));
+        System.out.println(l.longestPalindrome(s));
+        System.out.println(l.isPalindrome(s));
+    }
+}
---
+++ src/com/leetcode/chen/main/ReverseInteger/Solution.java
@@ -0,0 +1,43 @@
+/*
+ * $Header: Solution.java
+ * $Revision: 1.0.0.0
+ * $CreateDate: 2018年3月20日
+ * $ModifyDate: 2018年3月20日
+ * $Owner: LiuChen
+ * 
+ * Copyright (c) 2017-2027 ShangHai ChenJxx Co. Ltd.
+ * All Right Reserved.
+ */
+package com.leetcode.chen.main.ReverseInteger;
+
+/**
+ * Solution.java
+ * 
+ * @author LiuChen
+ * @version 1.0.0.0 2018年3月20日
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        int x = 123;
+        Solution solution = new Solution();
+        int result = solution.reverse(x);
+        System.out.println(result);
+    }
+
+    public int reverse(int x) {
+        int r = 0, result = 0, temp = 0;
+        int positive = x>=0 ? 1:-1;
+        x = Math.abs(x);
+        while (x > 0) {
+            r = x % 10;
+            x /= 10;
+            temp = result * 10;
+            if (temp / 10 != result) {
+                return 0;
+            }
+            result = temp + r;
+        }
+        return positive * result;
+    }
+}
